53279617d824db9d44d175e4490ff7c68c52aa1d
Taesung Hwang <taesunghwang@yahoo.com>
2020-11-15 22:54:33 -0800

    created basic word conversion interface

---
+++ src/components/converter.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import convert from "../services/word_transform";
+
+const words = new Set([]);
+
+const Converter = function() {
+
+  const [startWord, setStartWord] = useState("");
+  const [endWord, setEndWord] = useState("");
+
+  const [path, setPath] = useState([""]);
+  const [index, setIndex] = useState(0);
+
+  const startConversion = () => {
+    const conversion = convert(startWord, endWord, words);
+    setIndex(0);
+    setPath(conversion);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    startConversion();
+  }
+
+  const prevIndex = () => {
+    if (index > 0) {
+      setIndex(index => index - 1)
+    }
+  }
+  const nextIndex = () => {
+    if (index < path.length-1) {
+      setIndex(index => index + 1)
+    }
+  }
+
+
+  return (
+    <div className="converter">
+
+      <form onSubmit={handleSubmit}>
+
+        <label htmlFor="startWord">starting word</label>
+        <input
+          type="text"
+          id="startWord"
+          value={startWord}
+          onChange={(event) => {
+            setStartWord(event.target.value);
+          }}
+          placeholder="start word"
+        />
+
+        <span>to</span>
+
+        <label htmlFor="endWord">destination word</label>
+        <input
+          type="text"
+          id="endWord"
+          value={endWord}
+          onChange={(event) => {
+            setEndWord(event.target.value);
+          }}
+          placeholder="end word"
+        />
+
+        <button
+          type="submit"
+          disabled={!startWord || !endWord}
+        >
+          Convert
+        </button>
+
+      </form>
+
+      <h2>{path[index]}</h2>
+
+      <button onClick={prevIndex}>prev</button>
+      <button onClick={nextIndex}>next</button>
+
+    </div>
+  );
+};
+
+export default Converter;
